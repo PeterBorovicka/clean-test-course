11d316a261110fbff8a3d07f4c3955c30c9a9d56
PeterBorovicka <peter.borovicka1@gmail.com>
2025-11-12 07:59:40 +0100

    change

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/spec.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/spec.cy.js
@@ -1,35 +1,24 @@
-describe('Hits Home Page', () => {
-  it('passes', () => {
+describe('Shows Appeteasers', () => {
+  it('Connect to Dev Server', () => {
     cy.visit('https://hangryhippo.quantic.host/');
   });
-  it('displays the Welcome message', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are is a Welcome message.
-    cy.get('h1').should(
-      'have.text',
-      "Welcome to Hangry Hippo! Hungry? Let's get started!"
-    );
+  it('selects Handhelds', () => {
+    //Navigate to Handhelds.  This makes sure that the link actually exists.
+    cy.contains('Handhelds').click();
+    //Check if Cheese Burger is there.
+    cy.contains('Cheese Burger');
+    //Check if Fajita Tacos is there
+    cy.contains('Fajita Tacos');
   });
-  it('shows the Menu items', () => {
-    //Check for the Appeteasers menu item
-    cy.get('[data-testid=category-item]')
-      .should('be.visible')
-      .and('contain', 'Appeteasers');
-
-    //Check for the Handhelds menu item
-    cy.get('[data-testid=category-item]')
-      .should('be.visible')
-      .and('contain', 'Handhelds');
-  });
-});
-
-describe('Hits Order Page', () => {
-  it('passes', () => {
-    cy.visit('https://hangryhippo.quantic.host/order');
-  });
-  it('shows Your Order title', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    //Check to see if 'Your Order' exists
-    cy.get('h3').should('have.text', 'Your Order');
+  it('selects Appeteasers', () => {
+    //Navigate to Appeteasers.  This makes sure that the link actually exists.
+    cy.contains('Appeteasers').click();
+    //Check if Tater Tots is there.
+    cy.contains('Tater Tots');
+    //Check if Buffalo Wings is there.
+    cy.contains('Buffalo Wings');
+    //Make sure that the Handhelds DO NOT EXIST
+    cy.contains('Cheese Burger').should('not.exist');
+    cy.contains('Fajita Tacos').should('not.exist');
   });
 });
